5edc8ae1cd3d6e003c7debf8ed06d756df0d4776
Guillaume Esquevin <guillaume.esquevin@platypus-creation.com>
2011-08-14 17:54:51 +0200

    bumped up version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-draft",
-    version = "0.2.0",
+    version = "0.2.1",
     description = """
     Drafts for django admin
     """,
